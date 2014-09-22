514e1e67ec5a83143b1b215188b68983573da4f2
Wajahat <wajhat00@gmail.com>
2014-09-22 19:49:11 +0500

    First Migrate

---
+++ AdminRepo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AdminRepo.Global" Language="C#" %>
---
+++ AdminRepo/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AdminRepo.Startup))]
+namespace AdminRepo
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
